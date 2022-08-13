740cfd4b508b09bb4e61effa572c156119570f53
Pascal Klaßen <p.klassen221199@gmail.com>
2022-08-13 18:57:14 +0200

    Project setup

--- src/main/java/de/dummtrottel/magical_minerals/MagicalMinerals.java
+++ src/main/java/de/dummtrottel/magical_minerals/MagicalMinerals.java
@@ -1,7 +1,19 @@
 package de.dummtrottel.magical_minerals;
 
 import net.minecraftforge.fml.common.Mod;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
 
-@Mod("magical_minerals")
+@Mod(MagicalMinerals.ID)
 public class MagicalMinerals {
+
+    public static final String ID = "magical_minerals";
+    public static final String NAME = "Create: Magical Minerals";
+    public static final String VERSION = "1.18.2-0.0.0.1";
+
+    public static final Logger LOGGER = LogManager.getLogger();
+
+    public MagicalMinerals()
+    {
+    }
 }
